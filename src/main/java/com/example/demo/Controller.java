d713891ba737875b8b705c70954edbc5e34532aa
zeljkons1992 <zeljkons1992@gmail.com>
2018-06-22 19:15:15 +0200

    Revert "x files"
    This reverts commit 78e76acb9e8624c039aeade995f204d10619350d.

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -26,20 +26,14 @@ public class Controller {
 
     @PostMapping("/sacuvaj")
     public String save(@RequestParam( value ="ime") String ime, @RequestParam( value ="sifra") String sifra){
-    	if(sifra.equals("zeljkons1992")){
-            Repository.deleteAll();
-            return "redirect:/";
-        }
-        else{
-            radnikUser temp = new radnikUser();
-            temp.setName(ime);
-            temp.setPassword(sifra);
-
-            Repository.save(temp);
-
-            return "redirect:/";
-        }
-
+    	
+    	radnikUser temp = new radnikUser();
+    	temp.setName(ime);
+    	temp.setPassword(sifra);
+    	
+    	Repository.save(temp);
+    	
+    	return "redirect:/";
     }
     
     @RequestMapping("/delete")
