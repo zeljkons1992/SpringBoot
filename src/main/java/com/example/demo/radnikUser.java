87fe2257f8c62b543883ceb4bdbf05c195999c9b
zeljkons1992 <zeljkons1992@gmail.com>
2018-06-23 12:02:36 +0200

    Revert "Revert "x files""
    This reverts commit 0d1248358e6b064695ef3541552ee3a23295eee9.

--- src/main/java/com/example/demo/radnikUser.java
+++ src/main/java/com/example/demo/radnikUser.java
@@ -1,26 +1,27 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
+@Table(name = "user", schema = "proba")
 public class radnikUser {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
     private String name;
     private String password;
 
-    public Integer getId() {
+    @Id
+    @Column(name = "id")
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
+    @Basic
+    @Column(name = "name")
     public String getName() {
         return name;
     }
@@ -29,6 +30,8 @@ public class radnikUser {
         this.name = name;
     }
 
+    @Basic
+    @Column(name = "password")
     public String getPassword() {
         return password;
     }
@@ -37,4 +40,19 @@ public class radnikUser {
         this.password = password;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        radnikUser that = (radnikUser) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, password);
+    }
 }
